17f0ccf306eb4ed171f78556771aa36b61e652d3
choudry467 <62037033+choudry467@users.noreply.github.com>
2020-05-11 00:09:14 +0500

    RegexBasicClipboardDone

--- Regex Basic.py
+++ Regex Basic.py
@@ -1,13 +1,18 @@
-import re, paperclip
+
+import re
+import pyperclip
 
 #create regex for phone numbers
-phoneRegex = re.compile(r'''
-((\d\d\d) | (\(\d\d\d\)))?  # area code with or without paranthesis (optional)
 
-[a-zA-Z0-9_.+]+         #name part
-@           # @ symbol
-[a-zA-Z0-9_.+]+         #domain name part
-''', re.VERBOSE)
+
+phoneRegex = re.compile(r'''
+(((\d\d\d) | (\(\d\d\d\)))?  # area code with or without paranthesis (optional)
+(\s|-)      #space or dash
+(\d\d\d)    #first 3 digits
+(\s|-)      #space or dash
+(\d\d\d\d)    #last 4 digits
+(((ext(\.)?\s) | x)   #extension txt part
+(\d{2,5}))? )    #extension digit part''', re.VERBOSE)
 #Create regex for email addresses
 
 emailRegex = re.compile(r'''
@@ -18,5 +23,17 @@ emailRegex = re.compile(r'''
 ''', re.VERBOSE)
 
 #test of clipboard
-text = paperclip.paste()
+text = pyperclip.paste()
+
+#Extract
+extractedPhone = phoneRegex.findall(text)
+extractedEmail = emailRegex.findall(text)
+
+phoneNumbers = []
+for phoneNumber in extractedPhone:
+    phoneNumbers.append(phoneNumber[0])
+
+#copies results into clipboard
+pyperclip.copy('\n'.join(phoneNumbers) + '\n'.join(extractedEmail))
+
 
